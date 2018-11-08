c32d4124b9d24fdb435f2d89db147b788f174141
Andreas <Andreas.c.gade@gmail.com>
2018-11-08 15:40:05 +0100

    Added Camera movement relative to player

--- 2D Topdown Survival/Assets/Scripts/PlayerMovement.cs
+++ 2D Topdown Survival/Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,19 @@ public class PlayerMovement : MonoBehaviour {
 
     [SerializeField] private int movementSpeed;
 
+    Camera CC;
 
 
 	// Use this for initialization
 	void Start ()
     {
-
+        CC = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        CC.transform.position = new Vector3(transform.position.x, transform.position.y, CC.transform.position.z);
         transform.Translate(Input.GetAxisRaw("Horizontal") * Time.deltaTime * movementSpeed, Input.GetAxisRaw("Vertical") * Time.deltaTime * movementSpeed,0f);
 	}
 }
