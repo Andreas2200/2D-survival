96011e9a797bed086c92357925da202558800d4d
Andreas <Andreas.c.gade@gmail.com>
2018-11-08 14:57:29 +0100

    Made character movement
    Implemented character movement to our 2D player

---
+++ 2D Topdown Survival/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] private int movementSpeed;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Input.GetAxisRaw("Horizontal") * Time.deltaTime * movementSpeed, Input.GetAxisRaw("Vertical") * Time.deltaTime * movementSpeed,0f);
+	}
+}
