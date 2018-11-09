a8d7a639cf6a1f574c2a67b5b4aac004fe40852f
Andreas <Andreas.c.gade@gmail.com>
2018-11-09 11:15:45 +0100

    Implemented sprinting.

--- 2D Topdown Survival/Assets/Scripts/PlayerMovement.cs
+++ 2D Topdown Survival/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     [SerializeField] private int movementSpeed;
+    [SerializeField] private bool isRunning;
+
+    private int sprintSpeed = 1;
 
     Camera CC;
 
@@ -12,13 +15,32 @@ public class PlayerMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        isRunning = false;
         CC = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning = false;
+        }
+
+        if(isRunning)
+        {
+            sprintSpeed = 3;
+        }
+        else if(!isRunning)
+        {
+            sprintSpeed = 1;
+        }
+
         CC.transform.position = new Vector3(transform.position.x, transform.position.y, CC.transform.position.z);
-        transform.Translate(Input.GetAxisRaw("Horizontal") * Time.deltaTime * movementSpeed, Input.GetAxisRaw("Vertical") * Time.deltaTime * movementSpeed,0f);
-	}
+        transform.Translate(Input.GetAxisRaw("Horizontal") * Time.deltaTime * movementSpeed * sprintSpeed, Input.GetAxisRaw("Vertical") * Time.deltaTime * movementSpeed,0f);
+    }
 }
