b3f535533f4e810d0fe9e5bf95558330f4c4a9e2
Andreas <Andreas.c.gade@gmail.com>
2018-11-16 14:13:06 +0100

    Continued on the Inventory system
    Added UI elements to keep track of ressources

---
+++ 2D Topdown Survival/Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    InventoryManager IM;
+
+    public Text stoneCounter;
+    public Text treeCounter;
+
+	void Start ()
+    {
+        IM = FindObjectOfType<InventoryManager>();
+	}
+	
+
+	void Update ()
+    {
+        stoneCounter.text = "Stones: " + IM.stoneCount;
+        treeCounter.text = "Trees: " + IM.treeCount;
+	}
+}
